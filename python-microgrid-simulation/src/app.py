d2b00c3edbf79fcf44e510d7d4fa2e393f0569e6
Simon Andersen <smpa20@student.aau.dk>
2025-07-16 13:56:31 +0200

    changed simulation time

--- python-microgrid-simulation/src/app.py
+++ python-microgrid-simulation/src/app.py
@@ -360,7 +360,7 @@ def main():
     # microgrid.reset()
 
     wait_time = (
-        5.0  # 20 seconds to make the simulation 30x times faster than real time.
+        20.0  # 20 seconds to make the simulation 30x times faster than real time.
     )
     # starttime = time.monotonic()
 
